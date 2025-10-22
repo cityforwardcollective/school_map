a036d9c40d50a6688ebcb0ef007570b34e9c7db3
Spencer Schien <spencer.schien@gmail.com>
2025-10-22 15:05:15 -0500

    updated lots

--- R/1_data_prep.R
+++ R/1_data_prep.R
@@ -2,7 +2,8 @@ library(tidyverse)
 library(wisconsink12)
 library(scales)
 
-sy <- "2023-24"
+sy <- "2024-25"
+sy2 <- "2023-24"
 
 mps <- c("Traditional Public",
          "Instrumentality Charter",
@@ -229,11 +230,11 @@ prof_counts <- all_prof_tmp |>
 
 # saveRDS(all_prof, "../report_cards_2021-22/data/all_school_prof.rda")
 
-x <- all_prof |> 
+xx <- all_prof |> 
   pivot_wider(names_from = test_subject, values_from = pa) |> 
-  select(-accurate_agency_type) |> 
+  select(-c(accurate_agency_type, school_year)) |> 
   right_join(make_wi_rc(private_type = "all", exclude_milwaukee = FALSE) |> 
-               filter(school_year == "2023-24") |> 
+               filter(school_year == sy2) |> 
                select(dpi_true_id,
                       school_year,
                       school_name,
@@ -298,6 +299,48 @@ x <- all_prof |>
               select(dpi_true_id,
                      school_year,
                      milwaukee_indicator))
+
+cfc_scored <- map2_df(c("sch_ach", "sch_growth", "sch_tgo", "sch_ot"),
+        c("ach_weight", "growth_weight", "tgo_weight", "ot_weight"),
+        function(x, y) {
+          make_wi_rc(exclude_milwaukee = FALSE) |> 
+            ungroup() |> 
+            filter(school_year == sy2) |> 
+            select(school_year,
+                   dpi_true_id,
+                   value = x,
+                   weight = y) |> 
+            mutate(score = x)
+          # new_weight = ifelse(aw, half_weight / 2, weight))
+        }) |> 
+  group_by(school_year, 
+           dpi_true_id,
+           aw = score %in% c("sch_ach", "sch_growth")) |> 
+  mutate(half_weight = sum(weight, na.rm = TRUE) / 2,
+         new_weight = ifelse(aw, half_weight, weight),
+         nas = sum(is.na(weight))) |> 
+  ungroup() |> 
+  mutate(new_weight = ifelse(nas > 0, weight, half_weight)) |> 
+  arrange(dpi_true_id) |> 
+  ungroup() |> 
+  group_by(school_year, dpi_true_id) |> 
+  summarise(cfc_score = weighted.mean(value, new_weight, na.rm = TRUE) |> 
+              round(1)) |> 
+  ungroup() |> 
+  mutate(cfc_rating = case_when(
+    cfc_score < 48.0 ~ "Fails to Meet Expectations",
+    cfc_score < 58.0 ~ "Meets Few Expectations",
+    cfc_score < 70.0 ~ "Meets Expectations",
+    cfc_score < 83.0 ~ "Exceeds Expectations",
+    cfc_score < 100.0 ~ "Significantly Exceeds Expectations",
+    TRUE ~ NA
+  ))
+
+x <- left_join(xx, cfc_scored) |> 
+  rename(dpi_rating = overall_rating,
+         dpi_score = overall_score,
+         overall_rating = cfc_rating,
+         overall_score = cfc_score)
 
 saveRDS(x, "data/internal/prepped_data_1.rda")
 
